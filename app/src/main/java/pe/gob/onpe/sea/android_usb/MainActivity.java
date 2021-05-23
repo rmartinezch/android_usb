ee3321603c12f55d4ff8ba003db2ea257df56137
rmartinezch <rmartinezch@gmail.com>
2021-05-22 20:58:22 -0500

    Creation of the project

---
+++ app/src/main/java/pe/gob/onpe/sea/android_usb/MainActivity.java
@@ -0,0 +1,14 @@
+package pe.gob.onpe.sea.android_usb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
